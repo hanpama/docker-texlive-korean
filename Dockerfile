7c21618f0bfab39232d2e4d3896a16d6766d086c
Kyungil Choi <hanpama@gmail.com>
2015-11-02 14:44:23 +0900

    Write build.py

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,16 @@
-FROM ubuntu:14.04
+FROM ubuntu:15.10
 
 RUN apt-get update; \
     apt-get install -y xzdec texlive texlive-xetex texlive-luatex \
     texlive-lang-cjk texlive-lang-korean texlive-full; \
-    tlmgr --usermode init-usertree; \
-    tlmgr repository add http://ftp.ktug.org/KTUG/texlive/tlnet ktug; \
-    tlmgr pinning add ktug "*"; \
     apt-get -y clean; \
     rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*;
 
+RUN rm /usr/bin/python; \
+    apt-get update; \
+    apt-get install -y python3 python3-pip; \
+    pip3 install sphinx; \
+    apt-get -y clean; \
+    rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*;
+
+WORKDIR /var/code
---
+++ build.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+"""
+Builder
+
+Build sphinx korean document with hanpama/texlive-korean image!
+"""
+
+import argparse
+import os
+
+from source import conf
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("make_args")
+
+    project_name = conf.pfname
+    cwd = os.path.dirname(os.path.realpath(__file__))
+    make_args = parser.parse_args().make_args
+
+    os.system("""docker run --name={project_name} -it -v {cwd}:/var/code \
+    hanpama/texlive-korean make {make_args}""".format(
+        project_name=project_name, cwd=cwd, make_args=make_args)
+    )
+    print("DONE")
+    os.system("docker rm {project_name}".format(project_name=conf.pfname))
